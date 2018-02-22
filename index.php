440e221e09e102118dffc89f6003ea2de0a899bb
Sikorskiy Dmitriy <sikorskiy.dima@yandex.ru>
2018-02-22 14:29:46 +0200

    BRAB test task

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<meta charset="UTF-8">
+	<title>BRAB</title>
+	<meta name="description" content=""> 
+	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
+	<link rel="stylesheet" href="css/styles.min.css">
+</head>
+<body>
+<section class="program">
+	<div class="row">
+		<h3 class="section-title">что входит в программу?</h3>
+		<p class="section-subtitle">
+Программа «Здоровье спины при малоподвижном образе жизни» – это комплекс занятий по финско-немецкой
+методике DAVID под наблюдением профессионального реабилитолога на современных медицинских тренажерах, 
+оснащенных специальным компьютерным обеспечением. 
+		</p>
+		<div class="slider" id="slider">
+			<div class="slider-item"><img src="img/slider-img.jpg" alt=""></div>
+			<div class="slider-item"><img src="img/slider-img.jpg" alt=""></div>
+			<div class="slider-item"><img src="img/slider-img.jpg" alt=""></div>
+		</div>
+		<div class="program_list-wrap clearfix">
+			<ul class="program-list">
+				<li class="program-item">Первичное компьютерное тестирование для диагностики силы, амплитуды и состояния мышечного корсета и суставов.</li>
+				<li class="program-item">Составление индивидуальной программы занятий, с учетом полученных результатов тестирования</li>
+				<li class="program-item">Курс занятий по уникальной в Украине финско-немецкой методике на медицинских компьютеризированных тренажерах</li>
+			</ul>
+			<ul class="program-list">
+				<li class="program-item">Промежуточное и заключительное диагностическое исследование функций мышц и суставов, для контроля динамки положительного результата.</li>
+				<li class="program-item">Занятия проходят под обязательным контролем реабилитолога</li>
+			</ul>
+		</div>
+		<p class="porogram-text">
+Программу лечения в каждом конкретном случае составляет доктор во время первичной консультации.
+При наличии выраженных болей врач рекоменует курс лечения для снятия воспалительного процесса и отека и, после устранения 
+острой боли специалисты разрабатывают индивидуальную программу занятий</p>
+	</div>
+</section>
+<section class="sit">
+	<div class="row">
+		<h3 class="section-title">почему сидеть вредно?</h3>
+		<p class="section-subtitle">Около 85% людей в мире ведут малоподвижный образ жизни</p>
+		<p class="sit_text-top">Эта проблема является ключевой причиной различных заболеваний опорно-двигательного аппарата</p>
+		<ul class="sit-list clearfix">
+			<li class="sit-item">
+				<div class="sit_item-inner"><span class="sit-num">7,5</span>часов</div>
+				<span class="sit_item-sbt">Сидит
+				на работе</span>
+			</li>
+			<li class="sit-item">
+				<div class="sit_item-inner"><span class="sit-num">1,5</span>часа</div>
+				<span class="sit_item-sbt">Смотрит
+				телевизор</span>
+			</li>
+			<li class="sit-item">
+				<div class="sit_item-inner"><span class="sit-num">1,5</span>часа</div>
+				<span class="sit_item-sbt">Сидит
+				за компьютером</span>
+			</li>
+			<li class="sit-item">
+				<div class="sit_item-inner"><span class="sit-num">3</span>часа</div>
+				<span class="sit_item-sbt">Сидит, принимая
+				пищу</span>
+			</li>
+			<li class="sit-item">
+				<div class="sit_item-inner"><span class="sit-num">1,5</span>часа</div>
+				<span class="sit_item-sbt">Сидит
+				в транспорте</span>
+			</li>
+		</ul>
+		<p class="sit-count">сидя, современный человек проводит в среднем <span>15 часов</span> в день!</p>
+		<p class="section-subtitle">При сидении нагрузка на позвоночник возрастает в 11 раз</p>
+		<p class="sit-text">Когда человек садится, вся тяжесть тела ложится на позвоночник, провоцируя нарушение тонуса мышц и застой в тканях межпозвоночных дисков. Как результат – дискомфорт и болевые ощущения вдоль всего позвоночника.</p>
+	</div>
+</section>
+<section class="when">
+	<div class="row">
+		<h3 class="section-title">В каких случаях программа будет полезной?</h3>
+		<ul class="when-list">
+			<li class="when-item clearfix">
+				<div class="when-info">
+					<h4 class="when_info-title">Боли в пояснице</h4>
+					<p class="when_info-content">
+					Боли в поясничном отделе позвоночника связаны с его перенагрузкой в 80% случаев. 
+					Сидя за компьютером, даже в очень удобном офисном кресле, нагрузка на 
+					поясничный отдел практически в 2 раза больше, чем в  положении стоя.</p>
+					<a href="#" class="when_info-more">Подробнее ></a>
+				</div>
+				<div class="when-img"><img src="img/when-img1.jpg" alt=""></div>
+			</li>
+			<li class="when-item clearfix">
+				<div class="when-img"><img src="img/when-img2.jpg" alt=""></div>
+				<div class="when-info">
+					<h4 class="when_info-title">Боли в грудном отделе позвоночника</h4>
+					<p class="when_info-content">
+					Чаще всего дискомфорт в грудном отделе возникает из-за проблем с 
+					позвоночником. <a href="#" class="when_info-more">Подробнее ></a></p>
+				</div>
+			</li>
+			<li class="when-item clearfix">
+				<div class="when-info">
+					<h4 class="when_info-title">Боли в шее и плечах</h4>
+					<p class="when_info-content">
+					Очень часCто сидячая работа приводит к болевым ощущениям и дискомфорту  в плечах и 
+					шее. Человек, сидя за компьютером, склонив голову, вызывает колоссальное напряжение 
+					мышц шеи и воротниковой зоны. <a href="#" class="when_info-more">Подробнее ></a></p>
+					
+				</div>
+				<div class="when-img"><img src="img/when-img3.jpg" alt=""></div>
+			</li>
+			<li class="when-item clearfix">
+				<div class="when-img"><img src="img/when-img4.jpg" alt=""></div>
+				<div class="when-info">
+					<h4 class="when_info-title">Головные боли</h4>
+					<p class="when_info-content">
+					Более 50% офисных сотрудников испытывают головные боли. Около 90% из них  не в 
+					состоянии нормально функционировать при таком состоянии. Основная причина в 
+					большинстве случаев – слабые или зажатые мышц шейного отдела позвоночника.</p>
+					<a href="#" class="when_info-more">Подробнее ></a>
+				</div>
+			</li>
+		</ul>
+	</div>
+</section>
+<section class="callback">
+	<div class="row">
+		<h3 class="section-title">Записаться на прием</h3>
+		<p class="section-text">Позвоните в любое из наших отделений, ответим на все вопросы или запишем на прием к доктору</p>
+		<ul class="callback-list clearfix">
+			<li class="callback-item"><span class="city">Ивано-Франковск</span><span class="address">ул. Черновола</span><a href="tel:(0342) 74-45-40">(0342) 74-45-40</a></li>
+			<li class="callback-item"><span class="city">Киев</span><span class="address">м. Лукьяновка, КПИ, Осокорки, Позняки</span><a href="tel:(044) 574-88-86">(044) 574-88-86</a></li>
+			<li class="callback-item"><span class="city">Львов</span><span class="address">ул. Кульпарковская</span><a href="tel:(032) 254-03-46">(032) 254-03-46</a></li>
+		</ul>
+		<p class="section-text">Заполните форму и мы свяжемся с вами, ответим на все вопросы или запишем на прием к доктору</p>
+	</div>
+	<form action="#" class="callback-form">
+		<div class="callback_form-block">
+			<label>Отделение</label>
+			<div class="callback_select-wrap">
+				<select type="text" class="callback-input callback-select">
+					<option value="0" selected>Выберите удобное для вас отделение</option>
+				</select>
+			</div>
+			
+		</div>
+		<div class="callback_form-block">
+			<label for="">Имя</label>
+			<input type="text" placeholder="Имя" name="Name" class="callback-input callback-2">
+		</div>
+		<div class="callback_form-block">
+			<label for="">Телефон</label>
+			<input type="text" placeholder="+380" name="telephone" class="callback-input callback-3">
+		</div>
+		<input type="button" class="callback-submit" value="oтправить заявку" />
+	</form>
+</section>
+<!--  JQuery  -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(document).ready(function(){
+	$('#slider').slick({
+		arrows: true,
+		dots: true,
+		slidesToShow: 1,
+		prevArrow: '<button type="button" class="slick-prev"></button>',
+		nextArrow: '<button type="button" class="slick-next"></button>',
+		//appendArrows: $('.slider-nav')
+	});
+});
+
+	
+	
+	
+
